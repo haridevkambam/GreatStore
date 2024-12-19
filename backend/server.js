f81e5b5bf7655607c49700034e8f221214c7cabf
haridevkambam <haridevkambam@gmail.com>
2024-12-19 14:43:13 +0530

    add cart routes and create cart controllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(cookieParser()); // parse cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
