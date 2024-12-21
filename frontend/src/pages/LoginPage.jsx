6f6eedea30af9ade53056dedb42df74880cc41c1
haridevkambam <haridevkambam@gmail.com>
2024-12-21 20:32:54 +0530

    implement user authentication flow with loading state and redirects

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { LogIn, Mail, Lock, ArrowRight, Loader } from "lucide-react";
+import { useUserStore } from "../stores/useUserStore.js";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const loading = true;
+  const { login, loading } = useUserStore();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    // console.log(email, password);
+    login(email, password);
   }
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -48,7 +49,7 @@ const LoginPage = () => {
                   type='email'
                   required
                   value={email}
-                  onChange={(e) => setEmail({ email: e.target.value })}
+                  onChange={(e) => setEmail(e.target.value)}
                   className=' block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 
 									rounded-md shadow-sm
 									 placeholder-gray-400 focus:outline-none focus:ring-emerald-500 
@@ -71,7 +72,7 @@ const LoginPage = () => {
                   type='password'
                   required
                   value={password}
-                  onChange={(e) => setPassword({ password: e.target.value })}
+                  onChange={(e) => setPassword(e.target.value)}
                   className=' block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 
 									rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
                   placeholder='••••••••'
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore.js";
 
 const SignUpPage = () => {
-  const loading = false;
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -12,9 +12,12 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
+  const { signup, loading } = useUserStore();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    // console.log(formData);
+    signup(formData);
   }
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
