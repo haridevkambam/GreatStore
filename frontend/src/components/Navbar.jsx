c7e6b28ec0d6bbbdbf6da7950873f2af65c44310
haridevkambam <haridevkambam@gmail.com>
2024-12-21 14:14:30 +0530

    fix bugs

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,7 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
-// import { useUserStore } from "../stores/useUserStore";
-// import { useCartStore } from "../stores/useCartStore";
 
 const Navbar = () => {
-    // const { user, logout } = useUserStore();
-    // const isAdmin = user?.role === "admin";
-    // const { cart } = useCartStore();\
 
     const user = false;
     const isAdmin = true;
@@ -36,14 +31,7 @@ const Navbar = () => {
                             >
                                 <ShoppingCart className='inline-block mr-1 group-hover:text-emerald-400' size={20} />
                                 <span className='hidden sm:inline'>Cart</span>
-                                {/* {cart.length > 0 && (
-									<span
-										className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
-									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
-									>
-										{cart.length}
-									</span>
-								)} */}
+
                                 <span
                                     className='absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out'
@@ -67,7 +55,7 @@ const Navbar = () => {
                             <button
                                 className='bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
 						rounded-md flex items-center transition duration-300 ease-in-out'
-                                // onClick={logout}
+                            // onClick={logout}
                             >
                                 <LogOut size={18} />
                                 <span className='hidden sm:inline ml-2'>Log Out</span>
