82aa83f5728f5c769dae58ab63f9ef6ad0f26ecf
haridevkambam <haridevkambam@gmail.com>
2024-12-24 23:28:57 +0530

    update auth controller login status code

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -79,7 +79,7 @@ export const login = async (req, res)=>{
             });
         }
         else{
-            res.status(401).json({message : "Invalid credentials"});
+            res.status(400).json({message : "Invalid credentials"});
         }
     } catch (error) {
         console.log("Error in login controller", error.message);
